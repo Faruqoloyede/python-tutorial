ecffa0483841fee15857cb80097943b0ff9619f1
Faruqoloyede <oloyedefaruq2@gmail.com>
2026-07-02 15:22:53 +0100

    typecasting in python

---
+++ main.py
@@ -0,0 +1,12 @@
+
+user_name = "devfaruq"
+year = 2026
+email = 'devfaruq@fake.com'
+is_admin = True
+
+print(f"{user_name} started learning python in {year}")
+
+if(is_admin):
+    print(f"you are logged in as admin, your email is {email}")
+else: 
+    print(f"you are logged in as user, your email is {email}")
---
+++ typecasting.py
@@ -0,0 +1,26 @@
+ #typecasting
+
+name = ""
+age = 24
+gpa = 3.6
+student=True
+
+
+age = float(age)
+gpa = int(gpa)
+
+students = str(student)
+name = bool(name)
+# print(name)
+# print(type(students))
+# print(gpa)
+# print(age)
+
+x = 2.3
+y = 3
+
+division = x/y
+print(int(division))
+
+# x =y 
+# print(x)
