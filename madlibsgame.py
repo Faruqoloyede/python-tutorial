034b26c7ac10d5ee2087b8a6622b27a5f05ab1e3
Faruqoloyede <oloyedefaruq2@gmail.com>
2026-07-06 20:22:01 +0100

    practice

---
+++ madlibsgame.py
@@ -0,0 +1,10 @@
+name = str(input("Enter a name of a place: "))
+gender = str(input("enter a gender:"))
+age= int(input("enter an age:"))
+adjective = str(input("enter an adjective:"))
+adjective2 = str(input("enter another adjective:"))
+
+
+
+print(f"i went to the {name}, i saw a {gender} of {age}")
+print(f"i was {adjective} by his {adjective2} look")
