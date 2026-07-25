0a457b295a6cad56920a3c4b10d733fcffbf0bd3
Faruqoloyede <oloyedefaruq2@gmail.com>
2026-07-25 07:40:04 +0100

    collections in python

---
+++ collection.py
@@ -0,0 +1,40 @@
+fruits = ["apple", "orange", "banana", "coconut"]
+
+# print(fruits[::2])
+
+# for fruit in fruits:
+#     print(fruit)
+
+# print(len(fruits))
+# print("apple" in fruits)
+
+# fruits[1] = "pineapple"
+#
+# print(fruits)
+
+# fruits.append("cucumber")
+# fruits.remove("apple")
+# fruits.insert(2, "mango")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# fruits.count("apple")
+# print(fruits)
+
+#SETS{}
+states = {"kwara", "lagos", "Asaba", "osun"}
+
+# print(len(states))
+# states.add("Oyo")
+# states.remove("Asaba")
+# states.pop()
+# states.clear()
+# print(states)
+
+# TOPLE()
+
+foods = ("rice", "beans", "garri", "amala")
+
+# print(len(foods))
+# foods.count("rice")
+# print(foods)
